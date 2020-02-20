5e432f58f6f0ed8caaee27f3730a0563e9e3fd45
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2020-02-20 08:50:26 +0300

    Create Circle.java

---
+++ Lab4/LabPackets/ru.novsu.povt.gan/.graphics/Circle.java
@@ -0,0 +1,50 @@
+package ru.novsu.povt.gan.graphics;
+
+public class Circle extends Point {
+    private int r;
+    private String color;
+    private String name;
+
+    public Circle() {
+
+    }
+
+    @Override
+    public void moveTo(int x, int y) {
+        super.moveTo(x, y);
+    }
+
+    public void moveToRadius(int r) {
+        this.r = r;
+    }
+
+    public void moveToColor(String color) {
+        this.color = color;
+    }
+
+    public void moveToName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        super.move(dx, dy);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Для круга: ");
+        super.draw();
+        System.out.print("Радиус: ");
+        System.out.println("r = " + r);
+        System.out.println("Цвет: " + color);
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}

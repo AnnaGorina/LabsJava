5594d9accedcea5ba7f6b6d715754b9cc986c379
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2020-02-20 08:50:46 +0300

    Create Ellipse.java

---
+++ Lab4/LabPackets/ru.novsu.povt.gan/.graphics/Ellipse.java
@@ -0,0 +1,53 @@
+package ru.novsu.povt.gan.graphics;
+
+public class Ellipse extends Rectangle{
+    private int a;
+    private int b;
+
+    private String color;
+    private String name;
+
+    public Ellipse() {
+
+    }
+
+    @Override
+    public void moveTo(int x, int y) {
+        super.moveTo(x, y);
+    }
+
+    public void moveToOsi(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        super.move(dx, dy);
+    }
+
+    public void moveToColor(String color) {
+        this.color = color;
+    }
+
+    public void moveToName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Для эллипса: ");
+        super.draw();
+        System.out.print("Полуоси: ");
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Цвет: " + color);
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
